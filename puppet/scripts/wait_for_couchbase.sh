7d8a682f749c61bca5d1cea8dbf6010c5c74d0b0
David Carrera <dcarrera@ac.upc.edu>
2014-10-16 18:02:40 +0200

    Pools not created until node and cluster initialized

--- puppet/scripts/wait_for_couchbase.sh
+++ puppet/scripts/wait_for_couchbase.sh
@@ -1,10 +1,11 @@
 CB_STATUS_FILE=/tmp/cb_status.txt
 rm -f $CB_STATUS_FILE
+curl -s -X GET http://localhost:8091/pools | grep pools > $CB_STATUS_FILE
 status=`grep -s healthy $CB_STATUS_FILE | sed 's/ //g' | tail -1`
 while [ -z $status ]
 do
 	sleep 1
-	curl -s -X GET http://localhost:8091/pools/default | grep status > $CB_STATUS_FILE
+	curl -s -X GET http://localhost:8091/pools | grep pools > $CB_STATUS_FILE
 	status=`grep -s healthy $CB_STATUS_FILE | sed 's/ //g' | tail -1`
 done
 rm -f $CB_STATUS_FILE
