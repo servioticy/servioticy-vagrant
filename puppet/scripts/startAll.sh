aba6f07f8b00dc66e4ae1332334de467ceec93e3
David Carrera <dcarrera@ac.upc.edu>
2014-10-17 16:51:47 +0200

    change boot order

--- puppet/scripts/startAll.sh
+++ puppet/scripts/startAll.sh
@@ -4,8 +4,20 @@
 echo Starting servIoTicy services...
 echo "*******************************"
 
+sudo rm -f $ELASTICSEARCH_LOG_FILE
+sudo /etc/init.d/elasticsearch-serviolastic start &> /dev/null
+$SCRIPTS/wait_for_elasticsearch_up.sh
+
 sudo /etc/init.d/couchbase-server start &> /dev/null
 $SCRIPTS/wait_for_couchbase_up.sh 
+
+cd $USERDB_HOME
+python userDB.py &> /dev/null &
+$SCRIPTS/wait_for_userDB.sh
+
+sudo rm -rf $API_LOG_FOLDER/*
+sudo $API_HOME/bin/jetty.sh start &> /dev/null
+$SCRIPTS/wait_for_api.sh 
 
 sudo rm -f $BROKER_LOG_FILE
 sudo $SERVIBROKER_HOME/bin/apollo-broker run &> /dev/null &
@@ -20,18 +32,6 @@ cd $STORM_HOME
 rm -f $STORM_LOG_FILE
 bin/storm jar $DISPATCHER_HOME/dispatcher-0.4.0-jar-with-dependencies.jar com.servioticy.dispatcher.DispatcherTopology -f $DISPATCHER_HOME/dispatcher.xml &> $STORM_LOG_FILE &
 #$SCRIPTS/wait_for_storm.sh 
-
-cd $USERDB_HOME
-python userDB.py &> /dev/null &
-$SCRIPTS/wait_for_userDB.sh
-
-sudo rm -rf $API_LOG_FOLDER/*
-sudo $API_HOME/bin/jetty.sh start &> /dev/null
-$SCRIPTS/wait_for_api.sh 
-
-sudo rm -f $ELASTICSEARCH_LOG_FILE
-sudo /etc/init.d/elasticsearch-serviolastic start &> /dev/null
-$SCRIPTS/wait_for_elasticsearch_up.sh
 
 cd $BRIDGE_HOME
 echo Starting MQTT-REST Bridge...
