e22020e3b4ecc2f044cabb4ed8d4dfaa8e49d026
David Carrera <dcarrera@ac.upc.edu>
2014-10-17 16:01:22 +0200

    More logging for ES

--- puppet/scripts/wait_for_elasticsearch_up.sh
+++ puppet/scripts/wait_for_elasticsearch_up.sh
@@ -1,3 +1,4 @@
+echo Starting ElasticSearch service...
 sleep 2
 status=`grep -s started $ELASTICSEARCH_LOG_FILE | tail -1 | sed 's/ //g'`
 while [ -z $status ]
@@ -5,3 +6,4 @@ do
 	sleep 1
 	status=`grep -s started $ELASTICSEARCH_LOG_FILE | tail -1 | sed 's/ //g'`
 done
+echo ElasticSearch service running
