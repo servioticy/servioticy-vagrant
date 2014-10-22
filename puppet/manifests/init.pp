4e8b9a83a1231cc5bba2ab25d5566147a2a46de1
David Carrera <dcarrera@ac.upc.edu>
2014-10-22 11:56:03 +0200

    changes in the /data partition

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -2,19 +2,19 @@ include wget, git, apt
 
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin/"] }
 
-lvm::volume { 'data':
-  ensure => present,
-  vg     => 'data',
-  pv     => '/dev/sdb',
-  fstype => 'ext4',
-  size   => '1G',
-}->
-fstab { 'A test fstab entry':
-  source => '/dev/sdb1',
-  dest   => '/data',
-  type   => 'ext4',
-  before => File['/data/couchbase']
-}
+#lvm::volume { 'data':
+#  ensure => present,
+#  vg     => 'data',
+#  pv     => '/dev/sdb',
+#  fstype => 'ext4',
+#  size   => '1G',
+#}->
+#fstab { 'A test fstab entry':
+#  source => '/dev/sdb1',
+# dest   => '/data',
+#  type   => 'ext4',
+#  before => File['/data/couchbase']
+#}
 
 
 
