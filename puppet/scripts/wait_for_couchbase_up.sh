e63d395ea981f9d9a53b77fe3a7a1a83943b16aa
David Carrera <david.carrera@bsc.es>
2015-03-19 17:44:55 +0100

    one more update to scripts...
    one more chance to get it working

--- puppet/scripts/wait_for_couchbase_up.sh
+++ puppet/scripts/wait_for_couchbase_up.sh
@@ -3,9 +3,23 @@ rm -f $CB_STATUS_FILE
 status=`grep -s healthy $CB_STATUS_FILE | sed 's/ //g' | tail -1`
 while [ -z $status ]
 do
-	sleep 1
-	curl -s -X GET http://localhost:8091/pools/default | grep status > $CB_STATUS_FILE
-	status=`grep -s healthy $CB_STATUS_FILE | sed 's/ //g' | tail -1`
+        sleep 1
+        curl -s -X GET http://localhost:8091/pools/default | grep status > $CB_STATUS_FILE
+        status=`grep -s healthy $CB_STATUS_FILE | sed 's/ //g' | tail -1`
 done
 rm -f $CB_STATUS_FILE
 echo CouchBase service running
+
+for bucket in `echo serviceobjects soupdates actuations subscriptions`
+do
+	echo Checking $bucket bucket status
+	status=`grep -s healthy $CB_STATUS_FILE | sed 's/ //g' | tail -1`
+	while [ -z $status ]
+	do
+	        sleep 1
+	        curl -s -X GET http://localhost:8091/pools/default/buckets/$bucket | grep status > $CB_STATUS_FILE
+	        status=`grep -s healthy $CB_STATUS_FILE | sed 's/ //g' | tail -1`
+	done
+	rm -f $CB_STATUS_FILE
+	echo $bucket bucket healthy
+done
